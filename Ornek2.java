d734121f2e38d953f52df5aa50eae07ae2939b73
Emirkan Acar <nydaolian@gmail.com>
2020-01-07 21:48:54 +0300

    update

--- Ornek2.java
+++ Ornek2.java
@@ -3,19 +3,28 @@ package com.relakith;
 import java.util.Scanner;
 
 
-public class Ornek1 {
+public class Ornek2 {
 
     public static void main(String[] args) {
         Scanner k = new Scanner(System.in);
         int kisiSay, evliMaasOrt = 0, bekarYasOrt = 0, ortMaas = 0, menu, evliSayi = 0, bekarSayi = 0;
-        String data[][] = new String[0][5];
+
+        String isimler[] = new String[0];
+        String dogumYerleri[] = new String[0];
+        int dogumTarih[] = new int[0];
+        int medeniDurum[] = new int[0];
+        int maaslar[] = new int[0];
 
         System.out.println("Kaç kişi girelecek ?");
         kisiSay = k.nextInt();
 
         if (kisiSay > 0)
         {
-            data = new String[kisiSay][5];
+            isimler = new String[kisiSay];
+            dogumYerleri = new String[kisiSay];
+            dogumTarih = new int[kisiSay];
+            medeniDurum = new int[kisiSay];
+            maaslar = new int[kisiSay];
         }else {
             System.exit(0);
         }
@@ -25,17 +34,17 @@ public class Ornek1 {
             System.out.println("---------------");
             System.out.println(i+1 + ".Kişi");
             System.out.println("Adınızı girin: ");
-            data[i][0] = k.next();
+            isimler[i] = k.next();
 
             System.out.println("Doğum Yeriniz: ");
-            data[i][1] = k.next();
+            dogumYerleri[i] = k.next();
 
             System.out.println("Doğum Tarihiniz: ");
-            data[i][2] = k.next();
+            dogumTarih[i] = k.nextInt();
 
             System.out.println("Medeni Durumunuz(0=Bekar,1=Evli): ");
-            data[i][3] = k.next();
-            int mDurumTemp = Integer.parseInt(data[i][3]);
+            medeniDurum[i] = k.nextInt();
+            int mDurumTemp = medeniDurum[i];
 
             if(mDurumTemp == 0)
             {
@@ -46,13 +55,15 @@ public class Ornek1 {
             }
 
             System.out.println("Maaşınız: ");
-            data[i][4] = k.next();
-            int maasTemp = Integer.parseInt(data[i][4]);
+            maaslar[i] = k.nextInt();
+            int maasTemp = maaslar[i];
 
             if(mDurumTemp == 1)
             {
                 evliMaasOrt += maasTemp;
             }
+
+            ortMaas += maasTemp;
         }
 
         System.out.println("Menü: (1 Listeleme - 2 İstenilen Şehir Bilgisi - 3 Evlilerin Maaş Ort - 4 Bekar Yaş Ort - 5 Ort,Büyük,Küçük Maaş Ort)");
@@ -60,25 +71,30 @@ public class Ornek1 {
 
         switch (menu){
             case 1:
-                System.out.println("# \t İsim \t Doğum Yeri \t Doğum Tarihi \t Medeni Durum \t Maaş");
-                for (int i = 0; data.length > i; i++)
+                System.out.println("İsim \t Doğum Yeri \t Doğum Tarihi \t Medeni Durum \t Maaş");
+                for (int i = 0; isimler.length > i; i++)
                 {
-                    System.out.print(i + " \t ");
-                    for (int j = 0; data[i].length > j; j++)
-                    {
-                        System.out.print(data[i][j] + " \t ");
-                    }
+                    String medeniD = "";
+                    if(medeniDurum[i] == 0) medeniD = "Bekar";
+                    else medeniD = "Evli";
+
+                    System.out.print(isimler[i] + " \t " + dogumYerleri[i] + " \t " + dogumTarih[i] + " \t " + medeniD + " \t " + maaslar[i]);
+                    System.out.println();
                 }
                 break;
             case 2:
                 System.out.println("Şehir Girin: ");
                 String tempSehir = k.next();
 
-                for (int i = 0; data.length > i; i++)
+                for (int i = 0; isimler.length > i; i++)
                 {
-                    if(data[i][1].equalsIgnoreCase(tempSehir))
+                    if(dogumYerleri[i].equalsIgnoreCase(tempSehir))
                     {
-                        System.out.println("İsim: " + data[i][0] + " Doğum Yeri: " + data[i][1] + " Doğum Tarihi: " + data[i][2] + " Medeni Durum: " + data[i][3] + " Maaş: " + data[i][4]);
+                        String medeniD = "";
+                        if(medeniDurum[i] == 0) medeniD = "Bekar";
+                        else medeniD = "Evli";
+
+                        System.out.println("İsim: " + isimler[i] + " Doğum Yeri: " + dogumYerleri[i] + " Doğum Tarihi: " + dogumTarih[i] + " Medeni Durum: " + medeniD + " Maaş: " +  maaslar[i]);
                     }else {
                         System.out.println("Uygun Kayıt Bulunamadı!");
                     }
@@ -92,8 +108,8 @@ public class Ornek1 {
                 break;
             case 5:
                 System.out.println("Maaş Ortalaması: " + (ortMaas / kisiSay));
-                System.out.println("En büyük maaş: " + enBuyuk(data));
-                System.out.println("En küçük maaş: " + enKucuk(data));
+                System.out.println("En büyük maaş: " + enBuyuk(maaslar));
+                System.out.println("En küçük maaş: " + enKucuk(maaslar));
                 break;
             case 6:
                 System.exit(0);
@@ -114,32 +130,32 @@ public class Ornek1 {
 
 
     }
-    public static int enBuyuk(String[][] data)
+    public static int enBuyuk(int[] data)
     {
         int enBuyuk = 0;
         for (int i = 0; data.length > i; i++)
         {
-            int tempVal = Integer.parseInt(data[i][4]);
+            int tempVal = data[i];
             enBuyuk = tempVal;
-            if(Integer.parseInt(data[i][4]) > tempVal)
+            if(data[i]> tempVal)
             {
-                enBuyuk = Integer.parseInt(data[i][4]);
+                enBuyuk = data[i];
             }
         }
 
         return enBuyuk;
     }
 
-    public static int enKucuk(String[][] data)
+    public static int enKucuk(int[] data)
     {
         int enKucuk = 0;
         for (int i = 0; data.length > i; i++)
         {
-            int tempVal = Integer.parseInt(data[i][4]);
+            int tempVal = data[0];
             enKucuk = tempVal;
-            if(tempVal > Integer.parseInt(data[i][4]))
+            if(tempVal > data[i])
             {
-                enKucuk = Integer.parseInt(data[i][4]);
+                enKucuk = tempVal;
             }
         }
 
